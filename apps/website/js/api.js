66f6d0daefb5327f01724995c10c0777719d5f1c
Aremu Oluwagbamila <aremuoluwagbamila@gmail.com>
2024-03-28 23:36:36 +0100

    feat: payment flow api integration

---
+++ apps/website/js/api.js
@@ -0,0 +1,79 @@
+const BASE_URL = "http://127.0.0.1:3000";
+
+/**
+ *
+ * @param {string} path
+ * @param {object} query
+ */
+const get_request = async (path, query = {}) => {
+  if (!path) {
+    throw new Error("please pass a path param");
+  }
+  const queryKeys = Object.keys(query);
+  const hasQueryParams = queryKeys.length > 0;
+  const queryKeyWithValues = hasQueryParams
+    ? queryKeys.map((queryKey) => `${queryKey}=${query[queryKey]}`)
+    : [];
+  const query_params = hasQueryParams ? `?${queryKeyWithValues.join("&")}` : "";
+
+  let response = {};
+  try {
+    const request = await fetch(`${BASE_URL}${path}${query_params}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+    });
+
+    if (request.status == 200) {
+      const _response = await request.json();
+      response = _response["data"] ?? {};
+    } else {
+      console.warn("[api-get-request]", query?.message);
+    }
+  } catch (e) {
+    console.error("[api-get-request]", e.message);
+  } finally {
+    return response;
+  }
+};
+
+/**
+ *
+ * @param {string} path
+ * @param {object} body
+ */
+const post_request = async (path, body) => {
+  if (!path && !path.startsWith("/")) {
+    throw new Error("please pass a path param starting with /");
+  }
+  const hasBody = !!body;
+
+  let response = {};
+  try {
+    const request = await fetch(`${BASE_URL}${path}`, {
+      method: "POST",
+      ...(hasBody && { body: JSON.stringify(body) }),
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+    });
+
+    if (request.status == 200) {
+      response = await request.json();
+    } else {
+      console.warn("[api-post-request]", request?.message);
+    }
+  } catch (e) {
+    console.error("[api-post-request]", e.message);
+  } finally {
+    return response;
+  }
+};
+
+const api = {
+  get: get_request,
+  post: post_request,
+};
--- apps/website/js/index.js
+++ apps/website/js/index.js
@@ -4,16 +4,104 @@ const bankInfoForm = document.querySelector("#bank-info-form");
 const paymentSection = document.querySelector("#payment");
 const introSection = document.querySelector("#intro");
 
+const bankListInput = document.querySelector("#bank-list");
+const accountNumberInput = document.querySelector("#account-number");
+
+const userAccountNameSpan = document.querySelector("#user-account-name");
+
+window.addEventListener("load", async () => {
+  const bank_list = await api.get("/list_banks");
+  await setBankOptions(bank_list);
+});
+
+bankListInput.addEventListener("change", (e) => {
+  const selectedBank = e.target.value;
+
+  console.log({ selectedBank });
+
+  if (!selectedBank) {
+    accountNumberInput.setAttribute("disabled", "disabled");
+  } else {
+    accountNumberInput.removeAttribute("disabled");
+    accountNumberInput.focus();
+  }
+});
+
+const ACCOUNT_NUMBER_LENGTH = 10;
+accountNumberInput.addEventListener("keyup", async (e) => {
+  const accountNumber = e.target.value;
+
+  const lengthOfAccountNumber = accountNumber.length;
+
+  if (lengthOfAccountNumber === ACCOUNT_NUMBER_LENGTH) {
+    await verifyAccountDetails(accountNumber, bankListInput.value);
+  }
+});
+
+const setBankOptions = async (bank_list) => {
+  const noBankList = isObjectEmpty(bank_list);
+
+  if (noBankList) {
+    alert("Unable to fetch bank list, please refresh and trya gain");
+    return;
+  }
+
+  bank_list.forEach((bank) => {
+    const bankOption = document.createElement("option");
+    bankOption.innerText = bank.name;
+    bankOption.setAttribute("value", bank.code);
+
+    bankListInput.appendChild(bankOption);
+  });
+};
+
+const verifyAccountDetails = async (account_number, bank_code) => {
+  if (!account_number || !bank_code) {
+    console.error(
+      "[verifyAccountDetails], possible missing values: account_number, bank_code"
+    );
+    return;
+  }
+
+  const { account_name = "" } = await api.get("/validate_account_number", {
+    account_number,
+    bank_code,
+  });
+
+  if (!account_name) {
+    alert("No account detais, kindly check your account number and bank");
+    return;
+  }
+  userAccountNameSpan.innerText = account_name;
+};
+
+const isObjectEmpty = (object = {}) => {
+  Object.keys(object).length == 0;
+};
 consentForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  e.target.reset();
   hideElement(introSection);
   showElement(paymentSection);
 });
 
-bankInfoForm.addEventListener("submit", (e) => {
+bankInfoForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  hideElement(paymentSection);
-  showElement(introSection);
+
+  const { status } = await api.post("/pay", {
+    account_number: accountNumberInput.value,
+    bank_code: bankListInput.value,
+    name: userAccountNameSpan.innerText,
+  });
+
+  if (status === 200) {
+    alert("Transfer successful, kindly check your account");
+    e.target.reset();
+    hideElement(paymentSection);
+    showElement(introSection);
+  } else {
+    alert("Something went wrong, please try again");
+  }
 });
 
 const hideElement = (element) => {
