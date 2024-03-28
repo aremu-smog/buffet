e39cef0758c405659aedd8dacb721744eeb0d404
Aremu Oluwagbamila <aremuoluwagbamila@gmail.com>
2024-03-28 10:31:33 +0100

    feat: validate account details method

--- apps/santa-smog-api/app/controllers/payments_controller.rb
+++ apps/santa-smog-api/app/controllers/payments_controller.rb
@@ -23,16 +23,12 @@ class PaymentsController < ApplicationController
     handle_error e
   end
 
-  # TODO: Investigate why this is getting blocked by Cloudflare
-  def validate_account_number # rubocop:disable Metrics/AbcSize
+  def validate_account_number
     validate_params(expected_keys: %w[account_number bank_code], keys: params.keys)
 
-    paystack_url = URI("https://api.paystack.co/bank/resolve?account_number=#{params[:account_number]}&bank_code=#{params[:bank_code]}")
+    account_name = get_account_info(account_number: params[:account_number], bank_code: params[:bank_code])
 
-    req = Net::HTTP::Get.new(paystack_url)
-    req['Authorization'] = "Bearer #{ENV.fetch('PAYSTACK_SECRET_KEY')}"
-    res = Net::HTTP.start(paystack_url.hostname, paystack_url.port) { |http| http.request(req) }
-    render json: res
+    render json: { status: 200, data: { account_name: account_name } }
   rescue StandardError => e
     handle_error e
   end
@@ -77,12 +73,29 @@ class PaymentsController < ApplicationController
 
   def handle_error(error)
     status_code = error.message.split.first.to_i
-    render json: { status: status_code, message: error.message }
+    final_status_code = status_code.zero? ? 500 : status_code
+    render json: { status: final_status_code, message: error.message }
   end
 
   def validate_process_transaction_params(keys:)
     expected_keys = %w[name bank_code account_number]
     has_all_keys = expected_keys.all? { |key| keys.include?(key) }
     raise ArgumentError, "400 Bad Request. Expects: #{expected_keys.join(', ')} in body" unless has_all_keys
+  end
+
+  def get_account_info(account_number:, bank_code:)
+    paystack_url = URI("https://api.paystack.co/bank/resolve?account_number=#{account_number}&bank_code=#{bank_code}")
+
+    http = Net::HTTP.new(paystack_url.host, paystack_url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(paystack_url)
+    request['accept'] = 'application/json'
+    request['Authorization'] = "Bearer #{ENV.fetch('PAYSTACK_SECRET_KEY')}"
+
+    response = http.request(request)
+    response_body = JSON.parse(response.read_body)
+
+    response_body['data']['account_name']
   end
 end
