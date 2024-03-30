7eb517a63c92f49357dbfe4db17e841162bdfb36
Aremu Oluwagbamila <aremuoluwagbamila@gmail.com>
2024-03-30 22:00:55 +0100

    feat: setup webhook to topup or create donation after topping up paystack balance

--- apps/santa-smog-api/app/controllers/webhooks_controller.rb
+++ apps/santa-smog-api/app/controllers/webhooks_controller.rb
@@ -5,23 +5,40 @@ class WebhooksController < ApplicationController
     hash = OpenSSL::HMAC.hexdigest('SHA512', paystack_secret_key, request_body.to_json)
     return unless hash == request.headers['x-paystack-signature']
 
-    event = request_body['event']
-
-    render json: handle_paystack(event)
+    render json: handle_paystack(request_body)
   end
 
   private
 
-  def handle_paystack(event)
+  def handle_paystack(body) # rubocop:disable Metrics/MethodLength
     response = { status: 200 }
+
+    puts "[paystack-webhook], #{body.to_json}"
+    event = body['event']
     case event
     when 'transfer.success'
       puts 'Send an email to via a background job'
+    when 'charge.success'
+      response = { status: 500 } unless topup_or_create_donation(message: body['message'], amount: body['amount'])
     else
       response = { status: 500 }
       puts 'Event not recognized'
     end
 
     response
+  end
+
+  TRANSACTION_MESSAGE = 'santa smog'.freeze
+  def topup_or_create_donation(message:, amount:)
+    puts message
+    return false unless message.downcase.strip.eql? TRANSACTION_MESSAGE
+
+    last_donation = Donation.last
+    amount_per_recipient = ENV.fetch('AMOUNT_PER_RECIPIENT', 500_000)
+    no_of_recipients = (amount / amount_per_recipient).to_i
+    last_donation.update!({ no_of_recipients: no_of_recipients }) if last_donation.active
+
+    new_donation = Donation.create({ amount: amount_per_recipient, no_of_recipients: no_of_recipients, active: true })
+    new_donation.save
   end
 end
