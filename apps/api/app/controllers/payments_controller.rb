4ad2458907ffb7e3c267831b7c6aa3206e8257d3
Aremu Smog <aremuoluwagbamila@gmail.com>
2024-07-05 16:45:41 +0100

    do-not-convert-to-float

--- apps/api/app/controllers/payments_controller.rb
+++ apps/api/app/controllers/payments_controller.rb
@@ -97,7 +97,7 @@ class PaymentsController < ApplicationController # rubocop:disable Metrics/Class
     transfer.initializeTransfer(
       source: 'balance',
       reason: "Buffet for #{name}",
-      amount: amount_per_recipient.to_f,
+      amount: amount_per_recipient,
       recipient: recipient_code
     )
   rescue StandardError => e
