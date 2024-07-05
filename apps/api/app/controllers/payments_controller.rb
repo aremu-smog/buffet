aeb8ae2a9e036a230ce11c266b94e6efc33290f3
Aremu Smog <aremuoluwagbamila@gmail.com>
2024-07-05 16:31:46 +0100

    revert-to-expected-amount

--- apps/api/app/controllers/payments_controller.rb
+++ apps/api/app/controllers/payments_controller.rb
@@ -97,7 +97,7 @@ class PaymentsController < ApplicationController # rubocop:disable Metrics/Class
     transfer.initializeTransfer(
       source: 'balance',
       reason: "Buffet for #{name}",
-      amount: 5000,
+      amount: amount_per_recipient.to_f,
       recipient: recipient_code
     )
   rescue StandardError => e
